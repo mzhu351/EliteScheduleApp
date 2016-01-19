c7cb33d7c24ce34d59120ac5518d4a822ce20704
Min Zhu <mzhu11@Mins-iMac-592.local>
2016-01-18 20:37:10 -0800

    stuck on lodash .flatten

--- www/app/teams/team-detail-ctrl.js
+++ www/app/teams/team-detail-ctrl.js
@@ -1,11 +1,29 @@
 (function () {
   'use strict';
 
-  angular.module('eliteApp').controller('teamDetailCtrl', ['$stateParams', 'eliteApi',  teamDetailCtrl]);
+  angular.module('eliteApp').controller('TeamDetailCtrl', ['$stateParams', 'eliteApi',  TeamDetailCtrl]);
 
-  function teamDetailCtrl($stateParams) {
+  function TeamDetailCtrl($stateParams, eliteApi) {
     var vm = this;
-  console.log('$stateParams', $stateParams);
     vm.teamId = Number($stateParams.id);
+    var data = eliteApi.getLeagueData();
+    console.log('data', data);
+    var teams = _.chain(data.teams)
+                .flattenDeep('divisionTeams')
+        //        .find({"id": vm.teamId })
+                .value();
+console.log('teams',teams);//3222
+////console.log('teamId', vm.teamId);
+//var team = _.chain(teams).find('id': vm.teamId);
+  // vm.teamName = team.name;
+   //console.log('vm.teamName', vm.teamName);
+    // vm.games = _.chain(data.games)
+    //             .filter(isTeamInGame)
+    //             .map(function(item) {
+    //               var isTeam1 = (item.team1Id === vm.teamId ? true : false);
+    //               //var opponentName = isTeam1 ? item
+    //
+    //             })
+    //             .value();
   };
 })();
